5845f33a26aed7cfb358fa4379f661295f521963
rakib <rahmed@hvac.mea.com>
2024-02-11 16:56:56 -0500

    Ball Game

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    public GameObject Character; // The character game object
+    private Vector3 offset; // Offset distance between the camera and the character
+
+    void Start() {
+        // Calculate the initial offset by subtracting the character's position from the camera's position.
+        // Here, we access the Transform component of the Character to get its position.
+        offset = transform.position - Character.transform.position;
+    }
+
+    void LateUpdate() {
+        // Set the position of the camera to the character's position plus the offset.
+        // Again, accessing the Transform component of the Character to get its current position.
+        transform.position = Character.transform.position + offset;
+    }
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour {
+
+    public float jumpForce = 5f; // Stuff we want unity to show
+    public float restartThreshold = -5f;
+    public float torqueAmount = 1f;
+    private Rigidbody rb; // Stuff we dont want unity to show
+    private bool isGrounded;
+
+    void Start() {
+        Debug.Log("Movement.cs Script added to: " + gameObject.name); // Check to make sure we are connected to a game object
+        rb = GetComponent<Rigidbody>(); // Check for rigid body on the game object, then store the rigid body from the game object in a variable
+    }
+
+    void Update() {
+
+        float moveHorizontal = Input.GetAxis("Horizontal"); // When user clicks A or D // Left Arrow or Right Arrow
+        Vector3 torque = new Vector3(0f, 0f, -moveHorizontal) * torqueAmount; // 3 Axes Movement, X and Y are 0, but Z is our rotation
+        rb.AddTorque(torque); // Add it back to the game object rigid body which we have stored in a variable
+
+        if ((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        if (transform.position.y < restartThreshold) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+    }
+
+    void OnCollisionStay() {
+        isGrounded = true;
+    }
+}
